01bf8e94997fe8e2fd8fcf4856bf7f75191892aa
technoelectro <technoelectro.co@gmail.com>
2019-06-10 14:02:22 +0430

    Add About Me NiftyDialogEffects Library

--- app/src/main/java/com/example/myapplication/ui/MainActivity.java
+++ app/src/main/java/com/example/myapplication/ui/MainActivity.java
@@ -3,15 +3,20 @@ package com.example.myapplication.ui;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.airbnb.lottie.LottieAnimationView;
 import com.example.myapplication.R;
 import com.example.myapplication.utils.FontGuid;
+import com.gitonway.lee.niftymodaldialogeffects.lib.Effectstype;
+import com.gitonway.lee.niftymodaldialogeffects.lib.NiftyDialogBuilder;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -27,6 +32,7 @@ public class MainActivity extends AppCompatActivity {
     @BindView(R.id.tv_main_version)
     TextView tv_about;
 
+
     private Boolean flag = false;
 
     @Override
@@ -35,6 +41,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         initViews();
         lottieInit();
+
     }
 
     private void initViews() {
@@ -65,5 +72,22 @@ public class MainActivity extends AppCompatActivity {
                     startActivity(new Intent(MainActivity.this, NetworkCallActivity.class));
             }
         });
+    }
+
+    public void about(View view) {
+        NiftyDialogBuilder dialogBuilder=NiftyDialogBuilder.getInstance(this);
+
+        dialogBuilder
+                .withTitle(getString(R.string.about_me))                                  //.withTitle(null)  no title
+                .withTitleColor("#FFFFFF")                                  //def
+                .withDividerColor("#880E4F")                              //def
+                .withMessage(R.string.version_text)                     //.withMessage(null)  no Msg
+                .withMessageColor("#ffffff")                                //def  | withMessageColor(int resid)
+                .withDialogColor("#880E4F")                                 //def  | withDialogColor(int resid)
+                .withIcon(getResources().getDrawable(R.drawable.logo))
+                .withDuration(700)                                          //def
+                .withEffect(Effectstype.Slidetop)                          //def Effectstype.Slidetop
+                .isCancelableOnTouchOutside(true)                           //def  | isCancelable(true)
+                .show();
     }
 }
