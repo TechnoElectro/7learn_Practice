63e35bd1bad9061155bc71bdbb047ae9a077cbcc
technoelectro <technoelectro.co@gmail.com>
2019-06-18 22:28:26 +0430

    add Collection Frequency instead of old algorithm

--- app/src/main/java/com/example/myapplication/utils/TextHelper.java
+++ app/src/main/java/com/example/myapplication/utils/TextHelper.java
@@ -3,6 +3,8 @@ package com.example.myapplication.utils;
 import com.example.myapplication.model.WordCount;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class TextHelper {
@@ -22,30 +24,16 @@ public class TextHelper {
     }
 
     public static List<WordCount> getWordCount(String text) {
-        int repeatedWords;
-
         //Converts the string into lowercase
         String string = text.toLowerCase();
         List<WordCount> wordCountList = new ArrayList<>();
         //Split the string into words using built-in function
         String[] words = string.split(" ");
-
-        for (int i = 0; i < words.length; i++) {
-            repeatedWords = 1;
-            for (int j = i + 1; j < words.length; j++) {
-                if (words[i].equals(words[j])) {
-                    repeatedWords++;
-                    //Set words[j] to 0 to avoid printing visited word
-                    words[j] = "0";
-                }
-            }
-            //Displays the duplicate word
-            if (repeatedWords >= 1 && words[i] != "0") {
-                WordCount wordCount = new WordCount(words[i],repeatedWords);
-                wordCountList.add(wordCount);
-            }
+        for (String word : words) {
+            int frequency = Collections.frequency(Arrays.asList(words), word);
+              WordCount wordCount = new WordCount(word,frequency);
+              wordCountList.add(wordCount);
         }
-
         return wordCountList;
     }
 
